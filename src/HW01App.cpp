411cf8a12bcb30f874393beb15b937f1e08d1195
Adam Brush <brushat@gmail.com>
2012-09-05 08:23:48 -0400

    Moved some methods around and cleaned up the layout of the code
    Also added some more comments

--- src/HW01App.cpp
+++ src/HW01App.cpp
@@ -14,8 +14,7 @@
 * 
 * This project fulfills the following requirements for homework 1:
 * 
-* 
-*
+* A1, A2, A3, and A6 as well as C, D, E2, and E5.
 */
 
 #include "cinder/app/AppBasic.h"
@@ -48,18 +47,57 @@ private:
 	float brightnessR2_;
 	float brightnessG2_;
 	float brightnessB2_;
+	
 	int frameNumber_;
 	boost::posix_time::ptime app_start_time_;
 	Surface* mySurface_;
 	uint8_t* myPixels;
+	/**
+	* Draw a circle
+	* 
+	* This method will draw a circle based on the center point, radius, and color of the circle
+	* 
+	* It satisfies the draw circle method
+	*/
 	void drawCircle(uint8_t* pixels, int center_x, int center_y, int radius, Color8u fill);
+	/**
+	* Draw a rectangle
+	* 
+	* This method draws a rectangle based on the (x, y) coordinate of the top left corner of the rectangle,
+	* the side lengths of x and y and the color of the rectangle.  It assumes the user will not put negatives
+	* in for the coordinates or side lengths
+	* 
+	* It satisfies the rectangle requirement
+	*/
 	void drawRectangle(uint8_t* pixels,int start_x, int start_y, int rect_x, int rect_y, Color8u fill);
+	/**
+	* 
+	* Draw a line segment
+	* 
+	* This method draws a line segment based on the (x, y) coordinate of the start of the line and the length
+	* and color of the line
+	* 
+	* It satisfies the line segment requirement but doesn't have full functionality based on the fact that
+	* you can't make diagonal lines on the screen.
+	*/
 	void drawLineSegment(uint8_t* pixels, int x1, int y1, int length, Color8u fill);
-	void gradient(uint8_t* pixels, Color8u fill);
+	/**
+	* Clear the screen
+	* 
+	* This method will clear the entire screen to black.
+	* 
+	* Doesn't satisfy any requirements but did help me to initially implement the update functionality
+	*/
 	void clearBackground(uint8_t* pixels, int kAppWidth, int kAppHeight);
-	void makeLine( uint8_t* pixels, int x1, int y1, int x2, int y2, Color8u fill);
+	/**
+	* Adds a tint to the entire surface
+	* 
+	* This method adds a tint to the entire surface by accessing the pixel array and tinting it with
+	* the specified color.
+	* 
+	* Satisfies the tint requirement
+	*/
 	void tint(uint8_t* pixels, Color8u tint);
-	void drawTriangle(uint8_t* pixels, int a, int b, int c, Color8u fill);
 };
 
 void HW01App::prepSettings(Settings* settings) {
@@ -69,6 +107,7 @@ void HW01App::prepSettings(Settings* settings) {
 
 void HW01App::clearBackground(uint8_t* pixels, int kAppWidth, int kAppHeight){
     for(int i = 0; i<kAppWidth*kAppHeight*3; i++){
+		// set everything in the pixel array to black
         pixels[i] = 0;
     }
 }
@@ -94,13 +133,12 @@ void HW01App::drawCircle (uint8_t* pixels, int center_x, int center_y, int radiu
 			int dist = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
 			if(dist <= radius)
 			{
-					int offset = 3*(x + y*kSurfaceSize);
-					//pixels[offset] = fill.r;
-					//pixels[offset+1] = fill.g;
-					//pixels[offset+2] = fill.b;
-					pixels[offset] = pixels[offset]/2 + fill.r/2;
-					pixels[offset+1] = pixels[offset+1]/2 + fill.g/2;
-					pixels[offset+2] = pixels[offset+2]/2 + fill.b/2;
+					int index = 3*(x + y*kSurfaceSize);
+					// This is where I used some of Dr. Brinkmans code to make my circles transparent looking
+					// Satisfies the transparency requirement
+					pixels[index] = pixels[index]/2 + fill.r/2;
+					pixels[index+1] = pixels[index+1]/2 + fill.g/2;
+					pixels[index+2] = pixels[index+2]/2 + fill.b/2;
 			}
 		}
 	}
@@ -114,10 +152,11 @@ void HW01App::drawRectangle (uint8_t* pixels, int start_x, int start_y, int widt
 	{
 		for (int x = start_x; x <= (start_x + width); x++)
 		{
-			int offset = 3*(x + y*kSurfaceSize);
-			pixels[offset] = fill.r;
-			pixels[offset+1] = fill.g;
-			pixels[offset+2] = fill.b;
+			// Determine locations of
+			int index = 3*(x + y*kSurfaceSize);
+			pixels[index] = fill.r;
+			pixels[index+1] = fill.g;
+			pixels[index+2] = fill.b;
 		}
 	}
 }
@@ -132,9 +171,7 @@ void HW01App::drawLineSegment(uint8_t* pixels, int x1, int y1, int length, Color
 		x1 += 1;
 	}
 }
-void HW01App::drawTriangle(uint8_t* pixels, int a, int b, int c, Color8u fill){
-	//ints a, b and c coordinate to the 
-}
+
 void HW01App::setup()
 {
 	mySurface_ = new Surface(kSurfaceSize,kSurfaceSize,false);
@@ -149,6 +186,7 @@ void HW01App::update()
 {
 	myPixels = (*mySurface_).getData();
 	
+	// generates a random color
 	brightnessR1_ = (float)rand()/(float)RAND_MAX;
 	if(brightnessR1_<0.0f)
 		brightnessR1_ = 1.0f;
@@ -158,7 +196,7 @@ void HW01App::update()
 	brightnessB1_ = brightnessB1_ - 0.01f;
 	if(brightnessB1_<0.0f)
 		brightnessB1_ = 1.0f;
-	///
+	// generates a random color
 	brightnessR2_ = (float)rand()/(float)RAND_MAX;
 	if(brightnessR2_>1.0f)
 		brightnessR2_ = 0.0f;
@@ -168,29 +206,33 @@ void HW01App::update()
 	brightnessB2_ = brightnessB2_ + 0.2f;
 	if(brightnessB2_>1.0f)
 		brightnessB2_ = 0.0f;
-	
-			drawRectangle(myPixels, 0, 201, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
-			drawRectangle(myPixels, 401, 201, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
-			drawRectangle(myPixels, 201, 0, 200,200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
-			drawRectangle(myPixels, 601, 0, 199, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
-			drawRectangle(myPixels, 201, 401, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
-			drawRectangle(myPixels, 601, 401, 199, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
-			drawCircle(myPixels, 100, 100, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
-			drawCircle(myPixels, 300, 300, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
-			drawCircle(myPixels, 500, 100, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
-			drawCircle(myPixels, 700, 300, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
-			drawCircle(myPixels, 500, 500, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
-			drawCircle(myPixels, 100, 500, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
-			drawLineSegment(myPixels, 100, 100, 400, Color8u(200,50,50));
-			drawLineSegment(myPixels, 300, 300, 400, Color8u(200,50,50));
-			drawLineSegment(myPixels, 100, 500, 400, Color8u(200,50,50));
+			
+	// Draw the crazy checker board
+	// Satisfies the draw a picture/art requirement
+	drawRectangle(myPixels, 0, 201, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+	drawRectangle(myPixels, 401, 201, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+	drawRectangle(myPixels, 201, 0, 200,200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+	drawRectangle(myPixels, 601, 0, 199, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+	drawRectangle(myPixels, 201, 401, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+	drawRectangle(myPixels, 601, 401, 199, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+	drawCircle(myPixels, 100, 100, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+	drawCircle(myPixels, 300, 300, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+	drawCircle(myPixels, 500, 100, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+	drawCircle(myPixels, 700, 300, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+	drawCircle(myPixels, 500, 500, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+	drawCircle(myPixels, 100, 500, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+	drawLineSegment(myPixels, 100, 100, 400, Color8u(200,50,50));
+	drawLineSegment(myPixels, 300, 300, 400, Color8u(200,50,50));
+	drawLineSegment(myPixels, 100, 500, 400, Color8u(200,50,50));
 
 	// tint reacts interestingly because I am using the random number generator so it keeps redrawing in what looks like different colors
+	// eventhough tint is not being fed any of the random colors
 	tint(myPixels, Color8u(100,1,1));
 
 	if(frameNumber_ == 0){
-		writeImage("brushat.png",*mySurface_);
-		//We do this here, instead of setup, because we don't want to count the writeImage time in our estimate
+		// Same as Dr. Brinkman's code for saving
+		// Satisfies the save an image requirement
+		writeImage("crazycheckerboard.png",*mySurface_);
 		app_start_time_ = boost::posix_time::microsec_clock::local_time();
 	}
 	frameNumber_++;
