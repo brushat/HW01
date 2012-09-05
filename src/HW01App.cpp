59d30025ceb66bc40d60f5de6661aa8a87d04cdb
Adam Brush <brushat@gmail.com>
2012-09-04 21:56:37 -0400

    Added some functionallity to update()
    Still kind of stumped on how to do triangle, line, gradient.  Planning
    to figure out how to save the image next.

--- src/HW01App.cpp
+++ src/HW01App.cpp
@@ -14,6 +14,7 @@
 * 
 * This project fulfills the following requirements for homework 1:
 * 
+* 
 *
 */
 
@@ -22,6 +23,7 @@
 #include "cinder/gl/Texture.h"
 #include "cinder/ImageIo.h"
 #include "Resources.h"
+#include "boost/date_time/posix_time/posix_time.hpp"
 
 using namespace ci;
 using namespace ci::app;
@@ -39,18 +41,71 @@ private:
 	static const int kWinWidth = 800;
 	static const int kWinHeight = 600;
 	static const int kSurfaceSize=1024;
+	/// Brightness of the screen. Must be between 0.0f (black) and 1.0f (white), inclusive.
+	float brightnessR1_;
+	float brightnessG1_;
+	float brightnessB1_;
+	float brightnessR2_;
+	float brightnessG2_;
+	float brightnessB2_;
+	int frameNumber_;
+	boost::posix_time::ptime app_start_time_;
 	Surface* mySurface_;
 	uint8_t* myPixels;
-	void drawCircle(uint8_t* pixels, int center_x, int center_y, int radius, Color8u color);
+	void drawCircle(uint8_t* pixels, int center_x, int center_y, int radius, Color8u fill);
 	void drawRectangle(uint8_t* pixels,int start_x, int start_y, int rect_x, int rect_y, Color8u fill);
 	void gradient(uint8_t* pixels, Color8u fill);
+	void clearBackground(uint8_t* pixels, int kAppWidth, int kAppHeight);
+	void makeLine( uint8_t* pixels, int x1, int y1, int x2, int y2, Color8u fill);
+	void tint(uint8_t* pixels, Color8u tint);
 };
 
 void HW01App::prepSettings(Settings* settings) {
 	(*settings).setWindowSize(kWinWidth,kWinHeight);
-	(*settings).setResizable(false);
+	(*settings).setResizable(true);
 }
 
+void HW01App::clearBackground(uint8_t* pixels, int kAppWidth, int kAppHeight){
+    for(int i = 0; i<kAppWidth*kAppHeight*3; i++){
+        pixels[i] = 0;
+    }
+}
+
+void HW01App::makeLine ( uint8_t* pixels, int x0, int y0, int x1, int y1, Color8u c ){ 
+
+	 int     x, offset;       
+	 float   dy, dx, y, m;
+
+
+	//formula from geometry
+	 dy = y1 - y0;
+	 dx = x1 - x0;
+
+	 m = dy / dx;
+	 y = y0;
+
+	for ( x = x0; x <= x1; x++ ) {
+		int index = 3 * (x + (y * kSurfaceSize)); 
+     
+		 pixels [index]=  c.r;
+		 pixels [index+1]= c.g;
+		 pixels [index+2]= c.b; 
+      
+				  y += m;   //* Step y by slope m 
+			   }			// end for loop
+
+}
+void HW01App::tint(uint8_t* pixels, Color8u color)
+{	for(int y = 0; y < kWinHeight; y++){
+		for(int x = 0; x < kWinWidth; x++){	
+			int index = 3*(x + y*kSurfaceSize);
+
+			pixels[index]   += color.r;	
+			pixels[index+1] += color.g;	
+			pixels[index+2] += color.b;		
+		}	
+	}
+}
 
 void HW01App::drawCircle (uint8_t* pixels, int center_x, int center_y, int radius, Color8u fill){
 	for(int y = center_y - radius; y <= center_y + radius; y++)
@@ -89,10 +144,7 @@ void HW01App::drawRectangle (uint8_t* pixels, int start_x, int start_y, int widt
 void HW01App::setup()
 {
 	mySurface_ = new Surface(kSurfaceSize,kSurfaceSize,false);
-	myPixels = (*mySurface_).getData();
-	
-	
-	
+	frameNumber_ = 0;
 }
 
 void HW01App::mouseDown( MouseEvent event )
@@ -101,13 +153,56 @@ void HW01App::mouseDown( MouseEvent event )
 
 void HW01App::update()
 {
+	myPixels = (*mySurface_).getData();
 	
+	brightnessR1_ = (float)rand()/(float)RAND_MAX;
+	if(brightnessR1_<0.0f)
+		brightnessR1_ = 1.0f;
+	brightnessG1_ = brightnessG1_ + 0.01f;
+	if(brightnessG1_>1.0f)
+		brightnessG1_ = 0.0f;
+	brightnessB1_ = brightnessB1_ - 0.01f;
+	if(brightnessB1_<0.0f)
+		brightnessB1_ = 1.0f;
+	///
+	brightnessR2_ = (float)rand()/(float)RAND_MAX;
+	if(brightnessR2_>1.0f)
+		brightnessR2_ = 0.0f;
+	brightnessG2_ = brightnessG2_ + 0.01f;
+	if(brightnessG2_>1.0f)
+		brightnessG2_ = 0.0f;
+	brightnessB2_ = brightnessB2_ + 0.2f;
+	if(brightnessB2_>1.0f)
+		brightnessB2_ = 0.0f;
+	
+			drawRectangle(myPixels, 0, 201, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+			drawRectangle(myPixels, 401, 201, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+			drawRectangle(myPixels, 201, 0, 200,200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+			drawRectangle(myPixels, 601, 0, 199, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+			drawRectangle(myPixels, 201, 401, 200, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+			drawRectangle(myPixels, 601, 401, 199, 200, Color(brightnessR1_,brightnessB1_,brightnessG1_));
+			drawCircle(myPixels, 100, 100, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+			drawCircle(myPixels, 300, 300, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+			drawCircle(myPixels, 500, 100, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+			drawCircle(myPixels, 700, 300, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+			drawCircle(myPixels, 500, 500, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+			drawCircle(myPixels, 100, 500, 50, Color(brightnessR2_,brightnessB2_,brightnessG2_));
+			//drawLine(myPixels, 100, 1000, 1000, 500, Color8u(200,50,50));
+
+	// tint reacts interestingly because I am using the random number generator so it keeps redrawing in what looks like different colors
+	tint(myPixels, Color8u(100,1,1));
+
+	if(frameNumber_ == 0){
+		writeImage("brushat.png",*mySurface_);
+		//We do this here, instead of setup, because we don't want to count the writeImage time in our estimate
+		app_start_time_ = boost::posix_time::microsec_clock::local_time();
+	}
+	frameNumber_++;
 }
 
 void HW01App::draw()
 {
-	drawRectangle(myPixels, 0, 0, 200, 300, Color8u(1,200,200));
-	drawCircle(myPixels, 100, 100, 50, Color8u(200,200,1));
+	
 	gl::draw(*mySurface_);
 }
 
